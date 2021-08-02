0ed192bc76b166b922fc9d86ddffaf478ae23fd2
SilvijaSvegzde <83742823+SilvijaSvegzde@users.noreply.github.com>
2021-08-02 18:20:50 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -52,10 +52,10 @@ function displayForecast(response) {
       `
 
 <div class="row card card-block ">
-                <div class="col-4 weather-forecast-day">${formatDay(
+                <div class="col-5 weather-forecast-day">${formatDay(
                   forecastDay.dt
                 )}</div>
-                <div class="col-4" id="forecast-icon"><img src="http://openweathermap.org/img/wn/${forecastIcon}@2x.png" alt="" width="42" /></div>
+                <div class="col-2" id="forecast-icon"><img src="http://openweathermap.org/img/wn/${forecastIcon}@2x.png" alt="" width="42" /></div>
                 <div class="col-3 weather-forecast-temperatures"> <strong>${maxTemp}°</strong> ${minTemp}°</div>
             </div>
                 
